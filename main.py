5e70d5d197f98554902ffa7ce3de527e1ee88824
TNL2007 <Kamalnath.A@iiitb.ac.in>
2024-11-28 13:27:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import customtkinter
 import pymongo
 from Windows.RegisterWindow import RegisterWindow
